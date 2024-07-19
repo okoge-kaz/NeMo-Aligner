92b693cb9f6559493ddfb814a34183f06b882ee7
kazuki <kaz.tokyo.tech20@gmail.com>
2024-07-20 01:25:13 +0900

    env: install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Load modules
-module use ~/modulefiles
+module use /gs/fs/tga-NII-LLM/modules/modulefiles
 
 module load ylab/cuda/12.1
 module load ylab/cudnn/8.9.7
-module load ylab/nccl/cuda-12.1/2.18.3
+module load ylab/nccl/cuda-12.2/2.20.5
 module load ylab/hpcx/2.17.1
 module load ninja/1.11.1
 
-module load ffmpeg/6.1.1
+# module load ffmpeg/6.1.1
 
 pip install Cython
 
@@ -23,3 +23,24 @@ pip install wheel setuptools pip --upgrade
 pip install -r requirements.txt
 
 pip install nemo-aligner
+
+# install nvidia apex
+cd ..
+git clone git@github.com:NVIDIA/apex.git
+cd apex
+
+pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+
+# install transfoermer engine
+pip install git+https://github.com/NVIDIA/TransformerEngine.git@v1.6
+
+# install flash-atten
+pip uninstall flash-attn
+
+cd ..
+git clone git@github.com:Dao-AILab/flash-attention.git
+cd flash-attention
+
+git checkout v2.4.2
+
+pip install -e .
